69ade89bc3bb040444660b5f9a8f29b7e61d4db3
LeinaPulgarinT <leinatamayo@gmail.com>
2019-08-30 06:20:04 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//let izquierda = document.getElementById('izquierdaID');
+
+//izquierda.addEventListener('click', function(){
+    // document.write('SOY LA DIRECCIONAL IZQUIERDA');
+    //izquierda.style.background = 'red';
+    //izquierda.style.color = 'white';
+//});
